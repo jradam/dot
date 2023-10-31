f0fa094a3b5cdf404d2d1b51cd2908d13c6cb06f
jradam <15231996+jradam@users.noreply.github.com>
2023-10-31 20:55:01 +0000

    trialing no status

--- bash/bin/helpers.sh
+++ bash/bin/helpers.sh
@@ -58,8 +58,6 @@ g() {
 
   # If no arguments, show status/diff info and ask for commit message
   if [ -z "$1" ]; then
-    if ! gp status; then return 1; fi
-
     # if no changes, return here
     git diff-index --quiet HEAD
     if [ ! $? -ne 0 ]; then 
