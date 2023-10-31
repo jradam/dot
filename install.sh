01066ed0b1716173096bf8937c8c59fa74cc2a03
jradam <15231996+jradam@users.noreply.github.com>
2023-10-31 17:18:44 +0000

    silencing ssh keygen

--- install.sh
+++ install.sh
@@ -65,7 +65,7 @@ print "info" "Silencing login message"
 touch $HOME/.hushlogin
 
 print "info" "Generating SSH key for Gitlab"  
-yes "" | ssh-keygen -oq -t rsa -C "gitlab-ssh-key" -N ""
+yes "" | ssh-keygen -oq -t rsa -C "gitlab-ssh-key" -N "" > /dev/null
 
 print "info" "Creating secrets file"  
 touch $HOME/dotfiles/.env
