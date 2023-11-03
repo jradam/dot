20fd0b8bda382d3b142a641f399b6f1b544dd9dd
jradam <15231996+jradam@users.noreply.github.com>
2023-11-03 12:56:21 +0000

    notes

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -2,6 +2,7 @@ return {
   {
     "akinsho/git-conflict.nvim",
     version = "*",
+    -- TODO finish configuring this
     config = {
       list_opener = "copen",
       highlights = {
--- nvim/lua/plugins/movement.lua
+++ nvim/lua/plugins/movement.lua
@@ -1,28 +1,30 @@
 return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  opts = {
-    jump = {
-      autojump = true,
-    },
-    highlight = {
-      label = {
-        uppercase = false,
-        current = true,
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {
+      jump = {
+        autojump = true,
       },
-      backdrop = true,
+      highlight = {
+        label = {
+          uppercase = false,
+          current = true,
+        },
+        backdrop = true,
+      },
+      modes = {
+        char = { enabled = false },
+      },
     },
-    modes = {
-      char = { enabled = false },
-    },
-  },
-  keys = {
-    {
-      "s",
-      mode = { "n", "x", "o" },
-      function()
-        require("flash").jump()
-      end,
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+      },
     },
   },
 }
