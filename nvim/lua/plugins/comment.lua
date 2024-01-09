ed8f988f3ba139e87d29872aab312d3b14116441
jradam <15231996+jradam@users.noreply.github.com>
2024-01-09 22:29:01 +0000

    added ts commenting

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,4 +1,12 @@
 return {
 	"numToStr/Comment.nvim",
-	opts = {},
+	dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+	config = function()
+		require("ts_context_commentstring").setup({ enable_autocmd = false })
+
+		local integration = require("ts_context_commentstring.integrations.comment_nvim")
+		require("Comment").setup({
+			pre_hook = integration.create_pre_hook(),
+		})
+	end,
 }
