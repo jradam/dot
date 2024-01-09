4fb188bfd10bba568532b7f114e1c929797b3c47
jradam <15231996+jradam@users.noreply.github.com>
2024-01-09 02:01:39 +0000

    testing

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -126,6 +126,21 @@ return {
 			require("nvim-tree.api").tree.open()
 		end
 
+		-- Create a new autocommand group for NvimTree, if not already created
+		vim.api.nvim_create_augroup("NvimTreeAutoCmds", { clear = true })
+
+		-- Autocommand to refresh NvimTree when it becomes the focused buffer
+		vim.api.nvim_create_autocmd("BufEnter", {
+			group = "NvimTreeAutoCmds",
+			pattern = "NvimTree_*",
+			callback = function()
+				-- Check if the current buffer is NvimTree
+				if vim.bo.filetype == "NvimTree" then
+					require("nvim-tree.api").tree.refresh()
+				end
+			end,
+		})
+
 		vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_on_startup })
 	end,
 }
