b8719df1737ec597b1a2381ecaac41b2b0d3229d
jradam <15231996+jradam@users.noreply.github.com>
2024-01-09 11:18:30 +0000

    change XDG RUNTIME dir

--- nvim/lua/plugins/lsp/conform.lua
+++ nvim/lua/plugins/lsp/conform.lua
@@ -20,6 +20,9 @@ return {
 			return javascript()
 		end
 
+		-- Fixes issue with default XDG_RUNTIME_DIR being inaccessible
+		local XDG_RUNTIME_DIR = os.getenv("HOME") .. "/.temp-conform"
+
 		return {
 			formatters_by_ft = {
 				lua = { "stylua" },
@@ -34,11 +37,18 @@ return {
 			formatters = {
 				prettierd = {
 					env = {
+						XDG_RUNTIME_DIR = XDG_RUNTIME_DIR,
 						PRETTIERD_DEFAULT_CONFIG = vim.fn.expand(
 							os.getenv("HOME") .. "/.config/nvim/.config/.prettierrc.json"
 						),
 					},
 				},
+				eslint_d = {
+					env = {
+						XDG_RUNTIME_DIR = XDG_RUNTIME_DIR,
+					},
+				},
+
 				-- TODO Set up eslint formatting (and LSP?) to use own config if none found
 				-- https://github.com/3rd/linter
 			},
