99a4212852ddf92f7f314576737943806aad1899
jradam <15231996+jradam@users.noreply.github.com>
2024-01-10 21:20:23 +0000

    test

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -4,6 +4,8 @@ return {
 	config = function()
 		require("ts_context_commentstring").setup({ enable_autocmd = false })
 
+		-- test
+
 		local integration = require("ts_context_commentstring.integrations.comment_nvim")
 		require("Comment").setup({
 			pre_hook = integration.create_pre_hook(),
