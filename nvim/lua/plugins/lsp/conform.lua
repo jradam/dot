ee09a06deb35f57a17c7e3452d6b3088064515b2
jradam <15231996+jradam@users.noreply.github.com>
2024-01-10 20:57:46 +0000

    tidying

--- nvim/lua/plugins/lsp/conform.lua
+++ nvim/lua/plugins/lsp/conform.lua
@@ -1,39 +1,51 @@
 return {
-	"stevearc/conform.nvim",
-	opts = function()
-		-- Fixes issue with default XDG_RUNTIME_DIR being inaccessible
-		local XDG_RUNTIME_DIR = os.getenv("HOME") .. "/.temp-conform"
+  "stevearc/conform.nvim",
+  opts = function()
+    -- Fixes issue with default XDG_RUNTIME_DIR being inaccessible
+    local XDG_RUNTIME_DIR = os.getenv("HOME") .. "/.temp-conform"
 
-		-- Toggle formatting on save
-		vim.g.should_format = true
-		vim.keymap.set("n", "<leader>W", function()
-			vim.g.should_format = not vim.g.should_format
-			print("Formatting set to " .. tostring(vim.g.should_format))
-		end, { desc = "Toggle format" })
+    -- Toggle formatting on save
+    vim.g.should_format = true
+    vim.keymap.set("n", "<leader>W", function()
+      vim.g.should_format = not vim.g.should_format
+      print("Formatting set to " .. tostring(vim.g.should_format))
+    end, { desc = "Toggle format" })
 
-		return {
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { "prettierd" },
-				javascriptreact = { "prettierd" },
-				typescript = { "prettierd" },
-				typescriptreact = { "prettierd" },
-				json = { "prettierd" },
-			},
-			format_on_save = function()
-				if vim.g.should_format then
-					-- So LSP formatting (including eslint formatting) always runs
-					return { lsp_fallback = "always" }
-				end
-			end,
-			formatters = {
-				prettierd = {
-					env = {
-						XDG_RUNTIME_DIR = XDG_RUNTIME_DIR,
-						PRETTIERD_DEFAULT_CONFIG = vim.fn.stdpath("config") .. "/env/.prettierrc.json",
-					},
-				},
-			},
-		}
-	end,
+    local js_types = { "javascript", "javascriptreact", "typescript", "typescriptreact" }
+
+    -- Make LSP formatting (including eslint formatting) always run for JavaScript-types
+    local format_on_save = function()
+      local ft = vim.bo.filetype
+
+      if vim.g.should_format then
+        for _, filetype in ipairs(js_types) do
+          if ft == filetype then
+            return { lsp_fallback = "always" }
+          end
+        end
+        return { lsp_fallback = true }
+      end
+    end
+
+    local formatters_by_ft = {
+      lua = { "stylua" },
+      json = { "prettierd" },
+    }
+    for _, js_type in ipairs(js_types) do
+      formatters_by_ft[js_type] = { "prettierd" }
+    end
+
+    return {
+      formatters_by_ft = formatters_by_ft,
+      format_on_save = format_on_save,
+      formatters = {
+        prettierd = {
+          env = {
+            XDG_RUNTIME_DIR = XDG_RUNTIME_DIR,
+            PRETTIERD_DEFAULT_CONFIG = vim.fn.stdpath("config") .. "/env/.prettierrc.json",
+          },
+        },
+      },
+    }
+  end,
 }
