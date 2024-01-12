7de690ddc55ecf76eb6111bbae1f841bcad2f880
jradam <15231996+jradam@users.noreply.github.com>
2024-01-12 01:36:11 +0000

    simplify todo-comments

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -3,21 +3,7 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	opts = {
 		signs = false,
-		highlight = {
-			pattern = [[.*<(KEYWORDS)\s*:]],
-		},
-		search = {
-			pattern = [[\b(KEYWORDS):]],
-			-- TODO: what was this for?
-			-- args = {
-			-- 	"--color=never",
-			-- 	"--no-heading",
-			-- 	"--with-filename",
-			-- 	"--line-number",
-			-- 	"--column",
-			-- 	"--hidden",
-			-- 	"--glob=!.git",
-			-- },
-		},
+		highlight = { pattern = [[.*<(KEYWORDS)\s*:]] },
+		search = { pattern = [[\b(KEYWORDS):]] },
 	},
 }
