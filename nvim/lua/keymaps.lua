00c0ff11719361ba44716d42693d489aa512db9b
jradam <15231996+jradam@users.noreply.github.com>
2024-01-29 22:22:34 +0000

    Fixes back/del hotkeys

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -63,6 +63,5 @@ k("i", ";e", "=", { desc = "=" })
 k("i", ";c", ";", { desc = ";" })
 
 k("i", ";n", "<CR>", { desc = "Newline" })
--- TODO: make these do a word at a time
-k("i", ";b", "<Backspace>", { desc = "Backspace" })
-k("i", ";x", "<Delete>", { desc = "Delete" })
+k("i", ";b", "<C-w>", { desc = "Backspace word" })
+k("i", ";x", "<C-o>dw", { desc = "Delete word" })
