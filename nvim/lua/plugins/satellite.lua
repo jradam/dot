408b980d17c012ea0671a2182bfb625abe5192a2
jradam <15231996+jradam@users.noreply.github.com>
2024-01-29 16:41:54 +0000

    satellite still broken

--- nvim/lua/plugins/satellite.lua
+++ nvim/lua/plugins/satellite.lua
@@ -1,13 +1,14 @@
 return {
-	"lewis6991/satellite.nvim",
-	config = function()
-		-- This plugin crashes often but is very useful - pcall ignores the crashes
-		local status, satellite = pcall(require, "satellite")
-		if status then
-			---@diagnostic disable-next-line: missing-fields
-			satellite.setup({ current_only = true })
-		else
-			print("Failed to load satellite.nvim")
-		end
-	end,
+	-- FIXME:
+	-- "lewis6991/satellite.nvim",
+	-- config = function()
+	-- 	-- This plugin crashes often but is very useful - pcall ignores the crashes
+	-- 	local status, satellite = pcall(require, "satellite")
+	-- 	if status then
+	-- 		---@diagnostic disable-next-line: missing-fields
+	-- 		satellite.setup({ current_only = true })
+	-- 	else
+	-- 		print("Failed to load satellite.nvim")
+	-- 	end
+	-- end,
 }
