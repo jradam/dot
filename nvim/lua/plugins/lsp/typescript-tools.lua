da0c273658e4902bb9ca8e641b7ea5257dc8570d
jradam <15231996+jradam@users.noreply.github.com>
2024-01-29 18:26:08 +0000

    tstools fails when not working in project. added note

--- nvim/lua/plugins/lsp/typescript-tools.lua
+++ nvim/lua/plugins/lsp/typescript-tools.lua
@@ -15,3 +15,22 @@ return {
 		})
 	end,
 }
+
+-- FIXME: need to do something like this
+-- local function load_typescript_tools_if_package_json_exists()
+--     local path = vim.fn.getcwd() .. "/package.json"
+--     if vim.fn.filereadable(path) == 1 then
+--         require("typescript-tools").setup({
+--             -- your existing setup configuration
+--         })
+--     else
+--         -- Optionally, you can print a message or log this event
+--         vim.api.nvim_echo({{"typescript-tools.nvim not loaded - no package.json found", "WarningMsg"}}, true, {})
+--     end
+-- end
+--
+-- return {
+--     "pmizio/typescript-tools.nvim",
+--     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+--     config = load_typescript_tools_if_package_json_exists
+-- }
