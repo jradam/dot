49e6ceac5eec6d2a201e78c9b8168cfd43c3e7b6
jradam <15231996+jradam@users.noreply.github.com>
2024-01-29 19:20:46 +0000

    moved tailwind-sorter, added notes

--- nvim/lua/plugins/lsp/tailwind-sorter.lua
+++ nvim/lua/plugins/lsp/tailwind-sorter.lua
@@ -1,6 +1,11 @@
 return {
+	-- FIXME: Causes standalone js to load VERY slow
+	-- Crashes sometimes... still?
 	"laytan/tailwind-sorter.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-lua/plenary.nvim",
+	},
 	-- Takes a while to build on install/update, and if build is cancelled must reinstall
 	build = "cd formatter && npm i && npm run build",
 	config = true,
