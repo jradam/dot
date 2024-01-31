1b1f926ca05154041c4d39a374db445d9549b8c6
jradam <15231996+jradam@users.noreply.github.com>
2024-01-31 13:55:00 +0000

    hopefully fixed satellite crashing

--- nvim/lua/plugins/satellite.lua
+++ nvim/lua/plugins/satellite.lua
@@ -1,19 +1,10 @@
 return {
   "lewis6991/satellite.nvim",
   config = function()
-    -- This plugin crashes often but is very useful - pcall ignores the crashes
-    local status, satellite = pcall(require, "satellite")
-    if status then
-      ---@diagnostic disable-next-line: missing-fields
-      satellite.setup({ current_only = true })
-    else
-      print("Failed to load satellite.nvim")
-    end
+    vim.api.nvim_create_autocmd("FocusGained", {
+      pattern = "*",
+      command = ":SatelliteRefresh", -- To stop crashing when returning focus
+    })
+    require("satellite").setup()
   end,
 }
-
--- FIXME: When this breaks again, try some of this:
--- Try running disable then enable on every file open
--- The :SatelliteDisable command disables scrollbars.
--- The :SatelliteEnable command enables scrollbars. This is only necessary if scrollbars have previously been disabled.
--- The :SatelliteRefresh command refreshes the scrollbars. This is relevant when the scrollbars are out-of-sync, which can occur as a result of some window arrangement actions.
