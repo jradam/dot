9e44240889a0e100ff4da72b83110a2719df217f
jradam <15231996+jradam@users.noreply.github.com>
2024-02-01 16:49:27 +0000

    will this fix satellite?

--- nvim/lua/plugins/satellite.lua
+++ nvim/lua/plugins/satellite.lua
@@ -1,7 +1,7 @@
 return {
   "lewis6991/satellite.nvim",
   config = function()
-    vim.api.nvim_create_autocmd("FocusLost", {
+    vim.api.nvim_create_autocmd({ "FocusLost", "FocusGained" }, {
       pattern = "*",
       command = ":SatelliteRefresh", -- To stop crashing when returning focus
     })
