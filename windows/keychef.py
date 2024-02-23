1c568a004da4a04358358a1cb2346fc2af4a7c79
jradam <15231996+jradam@users.noreply.github.com>
2024-02-23 15:49:08 +0000

    nearly fully implemented keychef

--- windows/keychef.py
+++ windows/keychef.py
@@ -2,6 +2,11 @@
 # In Windows, this means that semicolon does nothing, as F13 is not bound there, and it can now act as our new keyboard layer.
 # In Windows Terminal's settings.json, I have bound F13 to send semicolon again so I can use it as a leader key in neovim, and I have blocked it from typing anything with <Nop>
 
+# TODO: figure out how to type semicolon in Neovim while still having functionality
+# TODO: figure out how to make semi+e type "=" but also work well for opening tree
+# TODO: mouse movement
+
+
 from winput import (
     VK_2, VK_C, VK_E, VK_ESCAPE, VK_H, VK_N, VK_OEM_1, VK_OEM_MINUS, VK_OEM_PLUS, VK_Q, VK_RETURN, VK_U, WP_DONT_PASS_INPUT_ON, WM_KEYDOWN, WM_KEYUP, VK_F, VK_OEM_4, VK_SHIFT, VK_J, press_key, release_key, WP_UNHOOK, WP_STOP, hook_keyboard, wait_messages, KeyboardEvent, VK_OEM_6, VK_D, VK_9, VK_K, VK_S, VK_0, VK_L, VK_F13, VK_LSHIFT
 )
@@ -37,65 +42,54 @@ def handle_ingredients(event: KeyboardEvent):
         press_key(VK_OEM_4)
         release_key(VK_OEM_4)
         release_key(VK_SHIFT)
-        return WP_DONT_PASS_INPUT_ON
     if event.vkCode == VK_J and event.action == WM_KEYDOWN:  # J > }
         press_key(VK_SHIFT)
         press_key(VK_OEM_6)
         release_key(VK_OEM_6)
         release_key(VK_SHIFT)
-        return WP_DONT_PASS_INPUT_ON
     if event.vkCode == VK_D and event.action == WM_KEYDOWN:  # D > (
         press_key(VK_SHIFT)
         press_key(VK_9)
         release_key(VK_9)
         release_key(VK_SHIFT)
-        return WP_DONT_PASS_INPUT_ON
     if event.vkCode == VK_K and event.action == WM_KEYDOWN:  # K > )
         press_key(VK_SHIFT)
         press_key(VK_0)
         release_key(VK_0)
         release_key(VK_SHIFT)
-        return WP_DONT_PASS_INPUT_ON
     if event.vkCode == VK_S and event.action == WM_KEYDOWN:  # S > [
         press_key(VK_OEM_4)
         release_key(VK_OEM_4)
-        return WP_DONT_PASS_INPUT_ON
     if event.vkCode == VK_L and event.action == WM_KEYDOWN:  # L > ]
         press_key(VK_OEM_6)
         release_key(VK_OEM_6)
-        return WP_DONT_PASS_INPUT_ON
     if event.vkCode == VK_U and event.action == WM_KEYDOWN:  # U > _
         press_key(VK_SHIFT)
         press_key(VK_OEM_MINUS)
         release_key(VK_OEM_MINUS)
         release_key(VK_SHIFT)
-        return WP_DONT_PASS_INPUT_ON
     if event.vkCode == VK_H and event.action == WM_KEYDOWN:  # H > -
         press_key(VK_OEM_MINUS)
         release_key(VK_OEM_MINUS)
-        return WP_DONT_PASS_INPUT_ON
     if event.vkCode == VK_Q and event.action == WM_KEYDOWN:  # Q > "
         press_key(VK_SHIFT)
         press_key(VK_2)
         release_key(VK_2)
         release_key(VK_SHIFT)
-        return WP_DONT_PASS_INPUT_ON
     if event.vkCode == VK_E and event.action == WM_KEYDOWN:  # E > =
         press_key(VK_OEM_PLUS)
         release_key(VK_OEM_PLUS)
-        return WP_DONT_PASS_INPUT_ON
     if event.vkCode == VK_C and event.action == WM_KEYDOWN:  # C > ;
         semicolon = True
         press_key(VK_OEM_1)
         release_key(VK_OEM_1)
         semicolon = False
-        return WP_DONT_PASS_INPUT_ON
     if event.vkCode == VK_N and event.action == WM_KEYDOWN:  # N > Return
         press_key(VK_RETURN)
         release_key(VK_RETURN)
-        return WP_DONT_PASS_INPUT_ON
     if event.vkCode == VK_ESCAPE and event.action == WM_KEYDOWN:  # Exit
         return WP_UNHOOK | WP_STOP
+    return WP_DONT_PASS_INPUT_ON
 
 
 def keyboard_callback(event: KeyboardEvent):
