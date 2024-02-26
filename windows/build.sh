fee098abd4a485eea43f6cc1eec24971ad4bc2ba
jradam <15231996+jradam@users.noreply.github.com>
2024-02-26 20:44:02 +0000

    moved keychef to public repo

--- windows/build.sh
+++ windows/build.sh
@@ -1,4 +1,3 @@
 # TODO: extend this to copy all windows files to correct place
-cp ~/dotfiles/windows/keychef.py /mnt/c/Users/adamj/Downloads
 cp ~/dotfiles/windows/terminal-settings.json /mnt/c/Users/adamj/AppData/Local/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/settings.json
 
