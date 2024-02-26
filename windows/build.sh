a6900e58b67487235c46ffcfe07e2bc84826b66e
jradam <15231996+jradam@users.noreply.github.com>
2024-02-26 20:50:46 +0000

    changing build

--- windows/build.sh
+++
@@ -1,3 +0,0 @@
-# TODO: extend this to copy all windows files to correct place
-cp ~/dotfiles/windows/terminal-settings.json /mnt/c/Users/adamj/AppData/Local/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/settings.json
-
