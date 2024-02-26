a2698eb33dc5476952e310db3982d0f3fa256184
jradam <15231996+jradam@users.noreply.github.com>
2024-02-26 20:51:05 +0000

    updating build

---
+++ windows/build.sh
@@ -0,0 +1,5 @@
+DOT=~/dotfiles/windows
+WIN=/mnt/c/Users/$USERNAME
+
+cp $DOT/terminal-settings.json $WIN/AppData/Local/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/settings.json
+
