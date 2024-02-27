2a4c5713668044145ed269d1bbe9fb403b31cdc8
jradam <15231996+jradam@users.noreply.github.com>
2024-02-27 17:33:08 +0000

    removing build file

--- windows/build.sh
+++
@@ -1,5 +0,0 @@
-DOT=~/dotfiles/windows
-WIN=/mnt/c/Users/$USERNAME
-
-cp $DOT/terminal-settings.json $WIN/AppData/Local/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/settings.json
-
