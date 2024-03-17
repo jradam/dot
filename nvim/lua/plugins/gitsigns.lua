376168211dffb42fc7bf823bd4bda5d2c3a47712
jradam <15231996+jradam@users.noreply.github.com>
2024-03-17 18:19:54 +0000

    removing keys from gitsigns

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,39 +1,12 @@
+-- NOTE: Keys for this in Spyglass
+
 return {
   "lewis6991/gitsigns.nvim",
-  opts = function()
-    local k = vim.keymap.set
-
-    k(
-      "n",
-      "<localleader>gg",
-      ":Gitsigns toggle_numhl<CR>",
-      { desc = "Toggle gitsigns" }
-    )
-    k(
-      "n",
-      "<localleader>gc",
-      ":Gitsigns change_base ",
-      { desc = "Change gitsigns base" }
-    )
-    k(
-      "n",
-      "<localleader>gb",
-      ":Gitsigns toggle_current_line_blame<CR>",
-      { desc = "Toggle blame" }
-    )
-    k(
-      "n",
-      "<localleader>gd",
-      ":Gitsigns toggle_deleted<CR>",
-      { desc = "Toggle gitsigns deleted" }
-    )
-
-    return {
-      current_line_blame_opts = {
-        virt_text_pos = "right_align",
-        delay = 0,
-      },
-      signcolumn = false,
-    }
-  end,
+  opts = {
+    current_line_blame_opts = {
+      virt_text_pos = "right_align",
+      delay = 0,
+    },
+    signcolumn = false,
+  },
 }
