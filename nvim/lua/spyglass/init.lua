05f513d97a9c25650aab330e94ecc42314fa6b12
jradam <15231996+jradam@users.noreply.github.com>
2024-03-21 21:03:31 +0000

    put ts-tools on easier shortcut

--- nvim/lua/spyglass/init.lua
+++ nvim/lua/spyglass/init.lua
@@ -9,4 +9,5 @@ local c = require("spyglass.configs")
 -- New: basic git commands on <C-g>
 
 k("n", "<leader>G", c.gitsigns, { desc = "Gitsigns" })
-k("n", "<leader>D", c.ts_tools, { desc = "TS Tools" })
+-- TODO: rebind what was <leader>d before
+k("n", "<leader>d", c.ts_tools, { desc = "TS Tools" })
