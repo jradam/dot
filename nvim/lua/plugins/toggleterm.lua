f774a586f67b9f808745273f8842f70ffa523eb1
jradam <15231996+jradam@users.noreply.github.com>
2024-08-03 06:27:58 -0400

    added new split terminals

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -3,13 +3,19 @@ return {
   opts = function()
     return {
       on_open = function() vim.cmd("startinsert!") end,
-      direction = "float",
       float_opts = {
         border = "curved",
-        -- winblend = 5, -- Some elements (emojis) don't blend
         width = math.floor(vim.api.nvim_win_get_width(0)),
         height = math.floor(vim.api.nvim_win_get_height(0)) - 1,
       },
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
+      persist_size = true,
     }
   end,
   init = function()
@@ -18,36 +24,52 @@ return {
     local Terminal = require("toggleterm.terminal").Terminal
 
     local terminal_one = Terminal:new({
+      direction = "float",
       highlights = {
         NormalFloat = { guibg = darkBg },
         FloatBorder = { guifg = c.green, guibg = darkBg },
       },
     })
     local terminal_two = Terminal:new({
+      direction = "float",
       highlights = {
         NormalFloat = { guibg = darkBg },
         FloatBorder = { guifg = c.red, guibg = darkBg },
       },
     })
+    local terminal_three = Terminal:new({ direction = "horizontal" })
+    local terminal_four = Terminal:new({ direction = "vertical" })
 
     function Toggle_terminal_one() terminal_one:toggle() end
     function Toggle_terminal_two() terminal_two:toggle() end
+    function Toggle_terminal_three() terminal_three:toggle() end
+    function Toggle_terminal_four() terminal_four:toggle() end
 
     vim.keymap.set(
       { "n", "t" },
       "<C-t>",
       "<cmd>lua Toggle_terminal_one()<CR>",
-      {
-        desc = "Terminal one",
-      }
+      { desc = "Terminal one" }
     )
     vim.keymap.set(
       { "n", "t" },
       "<C-y>",
       "<cmd>lua Toggle_terminal_two()<CR>",
-      {
-        desc = "Terminal two",
-      }
+      { desc = "Terminal two" }
+    )
+    -- TODO: Have these in a telescope picker instead
+    -- TODO: Need better bindings for moving between splits
+    vim.keymap.set(
+      { "n", "t" },
+      "<C-s>",
+      "<cmd>lua Toggle_terminal_three()<CR>",
+      { desc = "Terminal three" }
+    )
+    vim.keymap.set(
+      { "n", "t" },
+      "<C-e>",
+      "<cmd>lua Toggle_terminal_four()<CR>",
+      { desc = "Terminal four" }
     )
   end,
 }
