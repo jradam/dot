e5054bfe36865253b1a00b856a7e3fbfe600fb3b
jradam <15231996+jradam@users.noreply.github.com>
2024-08-27 11:30:13 -0400

    added todo

--- nvim/lua/spyglass/init.lua
+++ nvim/lua/spyglass/init.lua
@@ -4,6 +4,7 @@ local c = require("spyglass.configs")
 -- TODO:
 -- Make separate plugin
 -- Make <leader>r not open this somehow?
+-- Make a "recent changes" list - e.g. on main branch without changes, can get a list of files/changes ordered by most recent
 -- New: new version of TODO Telescope with nicer formatting
 -- New: replace easypick by implementing git helpers
 -- New: the output of :highlight
