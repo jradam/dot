22137599416cffeccca06c06fdacc098abb5fec8
jradam <15231996+jradam@users.noreply.github.com>
2025-01-09 10:54:49 -0500

    testing spider removal

--- nvim/lua/plugins/spider.lua
+++ nvim/lua/plugins/spider.lua
@@ -3,23 +3,23 @@ return {
   config = function()
     local k = vim.keymap.set
     -- Must be Ex-commands like this for dot-repeatability to work
-    k(
-      { "n", "o", "x" },
-      "w",
-      "<cmd>lua require('spider').motion('w')<CR>",
-      { desc = "Spider-w" }
-    )
-    k(
-      { "n", "o", "x" },
-      "e",
-      "<cmd>lua require('spider').motion('e')<CR>",
-      { desc = "Spider-e" }
-    )
-    k(
-      { "n", "o", "x" },
-      "b",
-      "<cmd>lua require('spider').motion('b')<CR>",
-      { desc = "Spider-b" }
-    )
+    -- k(
+    --   { "n", "o", "x" },
+    --   "w",
+    --   "<cmd>lua require('spider').motion('w')<CR>",
+    --   { desc = "Spider-w" }
+    -- )
+    -- k(
+    --   { "n", "o", "x" },
+    --   "e",
+    --   "<cmd>lua require('spider').motion('e')<CR>",
+    --   { desc = "Spider-e" }
+    -- )
+    -- k(
+    --   { "n", "o", "x" },
+    --   "b",
+    --   "<cmd>lua require('spider').motion('b')<CR>",
+    --   { desc = "Spider-b" }
+    -- )
   end,
 }
