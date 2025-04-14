131b17d48897f8191b5aab5d2fc2397a3c59d74a
jradam <15231996+jradam@users.noreply.github.com>
2025-04-14 11:35:26 -0400

    nvim-new wip

---
+++ nvim-new/helpers.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+M.allModes = {
+  "n", -- Normal
+  "v", -- Visual and Select
+  "s", -- Select
+  "x", -- Visual
+  "o", -- Operator-pending
+  "!", -- Insert and Command-line
+  "i", -- Insert
+  "l", -- ":lmap" mappings for Insert, Command-line and Lang-Arg
+  "c", -- Command-line
+  "t", -- Terminal-Job
+}
+
+return M
