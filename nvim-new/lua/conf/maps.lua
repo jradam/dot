1bb62006f6362696a702f79dcfa98bd980831874
jradam <15231996+jradam@users.noreply.github.com>
2025-05-30 10:20:40 -0400

    pre nvim switch

---
+++ nvim-new/lua/conf/maps.lua
@@ -0,0 +1,13 @@
+local k = vim.keymap.set
+
+-- Actions
+k("n", "<leader>w", function()
+  vim.cmd("checktime") -- check for changes before saving
+  vim.cmd("w")
+end, { desc = "Write" })
+
+-- Text manipulation
+k("n", "<leader>a", "ggVG", { desc = "Select all" })
+k("v", "<C-c>", [["+y]], { desc = "System clip" })
+k("v", "<leader>r", [[:s/\%V//g<Left><Left><Left>]], { desc = "Replace" })
+k("n", "K", "i<space><left>", { desc = "Insert space" })
