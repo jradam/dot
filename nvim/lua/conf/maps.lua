17cf15464fe2ca2b4029e69d095b454f85b00fc1
jradam <15231996+jradam@users.noreply.github.com>
2025-05-31 16:49:25 -0400

    wip

--- nvim/lua/conf/maps.lua
+++ nvim/lua/conf/maps.lua
@@ -5,7 +5,12 @@ k("n", "<leader>w", function()
   vim.cmd("checktime") -- check for changes before saving
   vim.cmd("w")
 end, { desc = "Write" })
+
 k("n","<leader>e", function() vim.cmd("NvimTreeToggle") end, {desc = "Tree"})
+
+vim.api.nvim_create_autocmd({"InsertEnter", "InsertLeave", "WinEnter", "BufEnter"}, {
+  callback = function() vim.cmd("checktime") end,
+}) -- Check for file changes when changing modes 
 
 -- Text manipulation
 k("n", "<leader>a", "ggVG", { desc = "Select all" })
