b0ff97e9778a6e47ed2712a5f8b5252501a81f23
jradam <15231996+jradam@users.noreply.github.com>
2025-06-04 20:40:37 -0400

    wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -62,31 +62,6 @@ vim.api.nvim_create_autocmd("FileType", {
   command = [[ setlocal formatoptions-=cro ]],
 })
 
--- FIXME: this broken!
--- No more helpfiles opening in splits. No more tabs either.
-vim.api.nvim_create_autocmd("BufWinEnter", {
-  pattern = "*",
-  callback = function(event)
-    local type = vim.bo[event.buf].filetype
-
-    if type == "help" or type == "markdown" or type == "text" then
-      vim.bo.buflisted = true -- Unhide from buffer list
-      vim.cmd.only() -- Put in own buffer
-    end
-
-    -- local tabs_open = vim.fn.tabpagenr("$")
-    -- local file_path = vim.fn.bufname(event.buf)
-
-    -- -- If a tab has appeared, close it and re-open the file normally
-    -- if tabs_open > 1 then
-    --   vim.defer_fn(function()
-    --     vim.cmd("tabclose")
-    --     vim.cmd("edit " .. file_path)
-    --   end, 10) -- Delay allows state to stabilise after tab open. Fixes bug where all buffers unfocus.
-    -- end
-  end,
-})
-
 require("lazy").setup({
   spec = { { import = "plug" } },
   install = { colorscheme = { "dracula" } },
