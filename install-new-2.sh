d74aa4b5e2e508e771807496fcb8e32f2c5cffd3
jradam <15231996+jradam@users.noreply.github.com>
2025-06-17 11:34:11 -0400

    updating install script

--- install-new-2.sh
+++ install-new-2.sh
@@ -2,9 +2,7 @@
 # - Install WSL with `wsl --install` (`wsl --unregister Ubuntu` to reset)
 # - Run this file with `git clone https://github.com/jradam/dot && ./dot/install.sh`
 
-# TMUX 
-sudo apt update 
-yes | sudo apt install build-essential tmux
+# TMUX
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -sf ~/dot/tmux/.tmux.conf ~/.tmux.conf
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
@@ -13,18 +11,23 @@ ln -sf ~/dot/tmux/.tmux.conf ~/.tmux.conf
 curl https://mise.run | sh
 mkdir -p ~/.config/mise
 ln -sf ~/dot/env/mise.toml ~/.config/mise/mise.toml
+ln -sf ~/dot/bash/.bashrc ~/.bashrc
+# touch dot/.env # TODO: Use mise for env stuff?
+source $HOME/.bashrc
 mise install
-
-# GITHUB
-yes | gh auth login --web
-git config --global core.excludesfile ~/dot/env/.gitignore
 
 # DIFF-SO-FANCY
 curl -L https://github.com/so-fancy/diff-so-fancy/releases/latest/download/diff-so-fancy -o ~/.local/bin/diff-so-fancy
 chmod +x ~/.local/bin/diff-so-fancy
 
+# GITHUB
+yes | gh auth login --web
+git config --global core.excludesfile ~/dot/env/.gitignore
+git config --global user.name "jradam"
+git config --global user.email "15231996+jradam@users.noreply.github.com"
+git config --global init.defaultBranch main
+git config --global pull.rebase false
+
 # FINAL
-touch dot/.env
+sudo apt update && yes | sudo apt install build-essential
 ln -sf ~/dot/nvim ~/.config/nvim
-ln -sf ~/dot/bash/.bashrc ~/.bashrc
-exec bash
